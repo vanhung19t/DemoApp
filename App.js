8cccb6e280c3556f775c83903149403e0577bf9c
vanhung19t <ngvanhungg1512@gmail.com>
2024-04-14 10:55:20 +0700

    Lab1

--- App.js
+++ App.js
@@ -10,17 +10,35 @@ import Homework  from "./src/HomeWork"
 import Login from "./src/Login"
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import DemoTheme from "./src/DemoTheme"
+import DemoIcon from "./src/DemoIcon"
+import DemoActivityIncator from "./src/DemoActivityIncator"
+import DemoAppbar from "./src/DemoAppbar"
 const Stack = createNativeStackNavigator();
+import { PaperProvider } from 'react-native-paper'
+
+import Project1 from './src/Lab1/Project1';
+import Project2 from './src/Lab1/Project2';
+import Project3 from './src/Lab1/Project3';
+import Project4 from './src/Lab1/Project4';
+import Project5 from './src/Lab1/Project5';
+import Project6 from './src/Lab1/Project6';
+import Project7 from './src/Lab1/Project7';
+import Project8 from './src/Lab1/Project8';
 
 export default function App(){
     return(
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-     </NavigationContainer>
+    //   <NavigationContainer>
+    //     <Stack.Navigator initialRouteName="Login">
+    //       <Stack.Screen name="Login" component={Login} />
+    //       <Stack.Screen name="Home" component={Home} />
+    //     </Stack.Navigator>
+    //  </NavigationContainer>
+    // <PaperProvider >
+    //   <DemoAppbar/>
+    // </PaperProvider>
+    // <Project1/>
+    <Project8/>
     )
    
 } 
--- src/Cat.js
+++ src/Cat.js
@@ -5,7 +5,7 @@ export default function Cat(props){
     const[isHungry,setHungry]=useState(true)
     return(
         <View>
-            <Text>Hello {props.name}, I am {isHungry?"hungry":"full"}</Text>
+            <Text style={{}}>Hello {props.name}, I am {isHungry?"hungry":"full"}</Text>
             <Button
                 onPress={()=>{setHungry(false)}}
                 disabled={!isHungry}
